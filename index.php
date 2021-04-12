2a7cbf40203132b06fa6e23ab8cc4296c1b3a7d6
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2021-04-12 19:45:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ echo "<h2>Hello World!</h2>";
 echo "Hello world!<br>";
 echo "This php website is served from ", gethostname(), "<br>";
 echo "Contents of /opt <br>";
-data = scandir("/opt");
+$data = scandir("/opt");
 print_r($data);
 ?> 
 
