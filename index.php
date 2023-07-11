22ee1a4f749cb723fd02c21a51c81941e3f75701
kelvinlnx <57615312+kelvinlnx@users.noreply.github.com>
2023-07-11 17:55:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php
 //header("Refresh: 3;");
-echo "<h2>Hello World!</h2>";
+echo "<h3>Hello World!</h3>";
 echo "Hello world!<br>";
 echo "This php website is served from ", gethostname(), "<br>";
 echo "Contents of /opt <br>";
